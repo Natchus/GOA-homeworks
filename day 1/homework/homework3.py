0e12a721888d76810e1ae1e1e3bfe69cbf65b4cf
Natchus <nataliatchubabria@gmail.com>
2025-02-22 20:08:37 +0400

    second commit

---
+++ day 1/homework/homework3.py
@@ -0,0 +1,21 @@
+name=("natalia")
+fruit=("apple")
+movie=("harry potter")
+
+age=(15)
+number=(10)
+money=(100000000)
+
+Pi=(3.14)
+pie=(0.5)
+float=(9.8)
+
+print(name)
+print(fruit)
+print(movie)
+print(age)
+print(number)
+print(money)
+print(Pi)
+print
+print
